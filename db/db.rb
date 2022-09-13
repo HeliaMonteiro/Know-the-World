fdc7d0a5de1e01e0061a550a6f8b7667c6e79809
helia.santos <helia.monteirods@gmail.com>
2022-09-13 18:04:04 +0930

    update pg connection

--- db/db.rb
+++ db/db.rb
@@ -1,7 +1,8 @@
 require 'pg'
 
 def run_sql(sql, sql_params = [])
-  db = PG.connect(dbname: 'know_the_world_db')
+  
+  db = PG.connect(ENV['DATABASE_URL'] || {dbname: 'know_the_world_db'})
   results = db.exec_params(sql, sql_params)
   db.close
   results
